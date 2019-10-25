d78dc507848f04f02bd09d4182b0f8fa1615b12a
karoline.bratz <karoline.bratz@192.168.56.1>
2019-10-24 21:07:29 -0300

    Primeiro Commit

---
+++ src/modelos/Usuario.java
@@ -0,0 +1,33 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelos;
+
+/**
+ *
+ * @author karoline.bratz
+ */
+public class Usuario {
+    
+    private String login;
+    private String nome;
+
+    public String getLogin() {
+        return login;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+    
+}
